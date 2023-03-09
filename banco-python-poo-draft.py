6f75b8282c98af16a2ba42aa26a49c8fb6a6a0aa
Guilherme Sousa <guilherme.sousa@engdb.com.br>
2023-03-09 14:01:42 -0300

    Correção de acordo com a PEP8

--- banco-python-poo-draft.py
+++ banco-python-poo-draft.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
-class Conta:
+
+class Conta: 
     def __init__(self, saldo, numero, agencia, cliente, historico):
         self._saldo = saldo
         self._numero = numero
@@ -21,35 +22,42 @@ class Conta:
     def nova_conta(cls, cliente, numero):
         return cls(cliente, numero)
 
+
 class ContaCorrente(Conta):
-    def __init__ (self, limite, limite_saques):
+    def __init__(self, limite, limite_saques):
         self._limite = limite
         self.limite_saques = limite_saques
 
+
 class Historico:
     def adicionar_tansacao(transacao):
-        transacao.registrar() #Transacao pode ser Deposito ou Saque
+        transacao.registrar()  # Transacao pode ser Deposito ou Saque
         return transacao
+
 
 class Transacao(ABC):
     @abstractmethod
     def registrar(self, conta):
         return conta
-    
+
+
 class Deposito(Transacao):
     def __init__(self, valor):
         self._valor = valor
+
     def registrar(self, conta):
-        #ESCREVER AQUI A ROTINA DE DEPOSITO com seu devido return
-        #return ?
+        # ESCREVER AQUI A ROTINA DE DEPOSITO com seu devido return
+        # return ?
         pass
+
 
 class Saque(Transacao):
     def __init__(self, valor):
         self._valor = valor
+
     def registrar(self, conta):
-        #ESCREVER AQUI A ROTINA DE SAQUE com seu devido return
-        #return ?
+        # ESCREVER AQUI A ROTINA DE SAQUE com seu devido return
+        # return ?
         pass
 
 
@@ -59,11 +67,13 @@ class Cliente:
         self._contas = contas
     
     def realizar_transacao(conta, transacao):
-        
-        #Consulta o saldo do cliente especifico na classe Conta e passa como valor na funcao seguinte FETCH DO VALOR DE SALDO PARA AQUELE CLIENTE
-        valor = saldo
-        transacao.registrar(valor)
+        """Consulta o saldo do cliente especifico na classe Conta e passa
+        como valor na funcao seguinte FETCH DO VALOR DE
+        SALDO PARA AQUELE CLIENTE """
+        # valor = saldo
+        # transacao.registrar(valor)
+        pass
 
     def adicionar_conta(conta):
         #
-        pass  
+        pass
